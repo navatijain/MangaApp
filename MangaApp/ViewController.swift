c00ab731731aebaf4a3b3dcbc39ddecb3ab5be51
Navati Jain <>
2021-02-10 13:34:42 -0500

    Initial Commit Removed storyboard

---
+++ MangaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MangaApp
+//
+//  Created by Navati Jain on 2021-02-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
